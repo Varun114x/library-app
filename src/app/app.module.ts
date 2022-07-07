d96574d686efa336a4207cf9024501cfe975eaa0
Varun114x <thampivarun114@gmail.com>
2022-07-07 23:10:51 +0530

    viewbook completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddbooksComponent } from './addbooks/addbooks.component';
 import { SearchbooksComponent } from './searchbooks/searchbooks.component';
+import { ViewbookComponent } from './viewbook/viewbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddbooksComponent,
-    SearchbooksComponent
+    SearchbooksComponent,
+    ViewbookComponent
   ],
   imports: [
     BrowserModule,
